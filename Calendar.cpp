9bf56c924a760be2dbc3fee70941e67c8ebc95b4
Woohyun Kim <95575122+W00hyun-Kim@users.noreply.github.com>
2022-04-14 09:36:53 +0900

    Add files via upload

--- Calendar.cpp
+++ Calendar.cpp
@@ -11,6 +11,7 @@ int checkMonth(int year) {
 int get_day_of_month(int year, int month) {
 	
 	int day_of_month[13] = {0,31,28,31,30,31,30,31,31,30,31,30,31};
+	//윤년인 날은 2월에 1더하기 
 	day_of_month[2] = day_of_month[2]+checkMonth(year);
 	
 	return day_of_month[month];
@@ -25,18 +26,22 @@ int get_day(int year, int month) {
 	for(int j=1; j<month; j++) {
 		past = past + get_day_of_month(year,j);
 	}
-	return (past+1) %7;  //1:월요일, 2:화요일........6:토요일, 7:일요일 
+	return (past) % 7+1;  //1:월요일, 2:화요일........6:토요일, 7:일요일 
 } 
 
 void printCal (int startDay, int endDay) {
 //	printf("%d",startDay);
 	printf(" 일 월 화 수 목 금 토 \n");
 	for(int i=0; i<startDay; i++) {
-		printf("   ");
+		if(startDay==7) {
+			printf("");
+		} else {
+			printf("   ");			
+		}
 	}
-	for(int day=1,line = startDay; day<=endDay; day++, line++) {
-		printf(" %2d",day);
-		if(line%7==6) {
+	for(int i=1,day = startDay; i<=endDay; i++, day++) {
+		printf(" %2d",i);
+		if(day%7==6) {
 			printf("\n");
 		}
 	}
